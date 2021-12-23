567ec19a9d3ff251e7a61f5044553c6b4e4eb0a1
jeoffreycanters <jeoffrey.canters@student.uantwerpen.be>
2021-12-23 11:24:19 +0100

    Changes by maxco

--- src/be/uantwerpen/fti/ei/UI/UIFrame.java
+++ src/be/uantwerpen/fti/ei/UI/UIFrame.java
@@ -7,7 +7,7 @@ import java.util.Observer;
 public class UIFrame extends JFrame implements Observer, ActionListener {
 
     public UIFrame(){
-        super("Max is een hoer");
+        super("Money Tracker");
         this.setSize(1000,1000);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
