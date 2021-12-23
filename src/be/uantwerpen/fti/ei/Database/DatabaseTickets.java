0931bc36b663e5fb8c39b303142f8f2377629ef0
jeoffreycanters <jeoffrey.canters@student.uantwerpen.be>
2021-12-23 11:40:16 +0100

    Changes by maxco

--- src/be/uantwerpen/fti/ei/Database/DatabaseTickets.java
+++ src/be/uantwerpen/fti/ei/Database/DatabaseTickets.java
@@ -1,13 +1,4 @@
 package be.uantwerpen.fti.ei.Database;
 
-import be.uantwerpen.fti.ei.Register_entry.TicketRegisterEntry;
-import be.uantwerpen.fti.ei.Tickets.Tickets;
-
-import java.util.Observable;
-
-
-public abstract class DatabaseTickets extends Observable{
-    public DatabaseTickets(){}
-    public abstract void addEntry(Tickets t, TicketRegisterEntry te);
-    public abstract TicketRegisterEntry getEntry(Tickets t);
+public class DatabaseTickets {
 }
