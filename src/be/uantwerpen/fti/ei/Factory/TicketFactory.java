51391672306ff19b89f16fcdf18ffa6ff8dd4fc1
jeoffreycanters <jeoffrey.canters@student.uantwerpen.be>
2021-12-23 11:40:04 +0100

    hoereeeeeeeeeeeeeeeeee

--- src/be/uantwerpen/fti/ei/Factory/TicketFactory.java
+++ src/be/uantwerpen/fti/ei/Factory/TicketFactory.java
@@ -3,18 +3,29 @@ package be.uantwerpen.fti.ei.Factory;
 import be.uantwerpen.fti.ei.Tickets.TicketEven;
 import be.uantwerpen.fti.ei.Tickets.TicketRandom;
 import be.uantwerpen.fti.ei.Tickets.Tickets;
+import be.uantwerpen.fti.ei.Users.User;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.UUID;
 
-public class TicketFactory {
-    public Tickets getTickets(String name, String function, int value){
-        if(Objects.equals(function,"Split even")){
-            return new TicketEven(name, value);
-        }
-        if(Objects.equals(function,"Split random")){
-            return new TicketRandom(name, value);
-        }
-        System.out.println("failed");
-        return null;
+public abstract class TicketFactory {
+    public abstract Tickets CreateTicket(String ticket, UUID user);
+
+    public Tickets CreateTicket(String ticket, UUID user, ArrayList<UUID> users){
+        Tickets ticketNew = CreateTicket(ticket, user);
+        //ticket1.setIndebted(users)
+        return ticketNew;
     }
+
+    public Tickets CreateTicket(String ticket, User user){
+        return CreateTicket(ticket, user); //user.getID()
+    }
+
+    public Tickets CreateTicket(String ticket, User user, ArrayList<UUID> users){
+        Tickets ticketNew = CreateTicket(ticket, user);
+        //ticketNew.setIndebted(users)
+        return ticketNew;
+    }
+
+    public static TicketFactory
 }
--- src/be/uantwerpen/fti/ei/Tickets/Tickets.java
+++ src/be/uantwerpen/fti/ei/Tickets/Tickets.java
@@ -1,37 +1,13 @@
 package be.uantwerpen.fti.ei.Tickets;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 public class Tickets {
-    private String name;
-    private String function;
-    private static int value;
+    protected String ticket;
+    protected UUID ID;
 
-    public Tickets(String name, String function, int value){
-        this.name = name;
-        this.function = function;
-        this.value = value;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public String getFunction(){
-        return function;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public void setFunction(String function){
-        this.function = function;
-    }
-
-    public static int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
 }
